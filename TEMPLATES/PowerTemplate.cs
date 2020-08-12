a42241b21fc4ad272e58364ebf064a1694680cec
Chris Gallardo <chrisgallardo@live.com>
2020-08-12 11:48:12 -0700

    Added Equipped bool to templates

--- TEMPLATES/PowerTemplate.cs
+++ TEMPLATES/PowerTemplate.cs
@@ -14,60 +14,65 @@ public class Power : ScriptableObject
     public bool IsAblity; //Is this power an ability that is activated by pressing a button?
     public int cooldown; //If this power is an ability, there needs to be a cooldown time. This is measured in seconds
 
+    public bool Equipped = false;
 
-    public void ItemEncountered()
-    {
-        //ItemEncountered will be called if this is a power, and it is an ability.
-        //It assigns a key to the ability.
-        //You probably don't need to add anything to this function, 
-        //but keep it here.
-        public int abilityKey = Random.Range(0, 3);
-    }
 
-    public void OnRoll()
+    if (Equipped)
     {
-        //OnRoll() will be called whenever the player rolls. 
-        //You don't have to worry about actually making the player roll.
-        //This is just for adding stuff like drop a bomb, or make fire.
-    }
-
-    public bool Ability(int button)
-    {
-        if (button == abilityKey)
+        public void ItemEncountered()
         {
-            //This will happen when the player activates the ability
-            return true; //This is needed so that the cooldown manager knows if something happened.
+            //ItemEncountered will be called if this is a power, and it is an ability.
+            //It assigns a key to the ability.
+            //You probably don't need to add anything to this function, 
+            //but keep it here.
+            public int abilityKey = Random.Range(0, 3);
         }
-    }
 
-    public void OnShoot()
-    {
-        //This happens when player shoots.
-        //This is usefull when you want something to happen every x shots.
-    }
+        public void OnRoll()
+        {
+            //OnRoll() will be called whenever the player rolls. 
+            //You don't have to worry about actually making the player roll.
+            //This is just for adding stuff like drop a bomb, or make fire.
+        }
 
-    public void OnSwing()
-    {
-        //This happens when player swingsa sword.
-        //This is usefull when you want something to happen every x swings.
-    }
+        public bool Ability(int button)
+        {
+            if (button == abilityKey)
+            {
+                //This will happen when the player activates the ability
+                return true; //This is needed so that the cooldown manager knows if something happened.
+            }
+        }
 
-    public void OnLifeGain(int LifeGained)
-    {
-        //This is called when the player gains life, 
-        //and in the parameters is the amount of life gained.
-    }
+        public void OnShoot()
+        {
+            //This happens when player shoots.
+            //This is usefull when you want something to happen every x shots.
+        }
 
-    public void OnDamaged(int damage)
-    {
-        //This is called when the player is damaged.
-        //The damage that is dealt will be passed through the parameters.
-        //This can be used for a "thorns" effect.
-    }
+        public void OnSwing()
+        {
+            //This happens when player swingsa sword.
+            //This is usefull when you want something to happen every x swings.
+        }
 
-    public void OnEnemyDeath()
-    {
-        //This is called when an enemy dies.
-        //This can be used for a looting effect.
+        public void OnLifeGain(int LifeGained)
+        {
+            //This is called when the player gains life, 
+            //and in the parameters is the amount of life gained.
+        }
+
+        public void OnDamaged(int damage)
+        {
+            //This is called when the player is damaged.
+            //The damage that is dealt will be passed through the parameters.
+            //This can be used for a "thorns" effect.
+        }
+
+        public void OnEnemyDeath()
+        {
+            //This is called when an enemy dies.
+            //This can be used for a looting effect.
+        }
     }
 }
--- TEMPLATES/WeaponTemplate.cs
+++ TEMPLATES/WeaponTemplate.cs
@@ -16,42 +16,46 @@ public class Weapon : ScriptableObject
     public int moveSpeed; //The player's base movement speed when using this weapon
     public int attackSpeed; //This determines the base amount of time between attacks when using this weapon
 
+    public bool Equipped = false;
 
-    public void OnRoll()
+    if (Equipped)
     {
-        //OnRoll() will be called whenever the player rolls. 
-        //You don't have to worry about actually making the player roll.
-        //This is just for adding stuff like drop a bomb, or make fire.
-    }
+        public void OnRoll()
+        {
+            //OnRoll() will be called whenever the player rolls. 
+            //You don't have to worry about actually making the player roll.
+            //This is just for adding stuff like drop a bomb, or make fire.
+        }
 
-    public void OnShoot()
-    {
-        //This happens when player shoots.
-        //This is usefull when you want something to happen every x shots.
-    }
+        public void OnShoot()
+        {
+            //This happens when player shoots.
+            //This is usefull when you want something to happen every x shots.
+        }
 
-    public void OnSwing()
-    {
-        //This happens when player swings a weapon.
-        //This is usefull when you want something to happen every x swings.
-    }
+        public void OnSwing()
+        {
+            //This happens when player swings a weapon.
+            //This is usefull when you want something to happen every x swings.
+        }
 
-    public void OnLifeGain(int LifeGained)
-    {
-        //This is called when the player gains life, 
-        //and in the parameters is the amount of life gained.
-    }
+        public void OnLifeGain(int LifeGained)
+        {
+            //This is called when the player gains life, 
+            //and in the parameters is the amount of life gained.
+        }
 
-    public void OnDamaged(int damage)
-    {
-        //This is called when the player is damaged.
-        //The damage that is dealt will be passed through the parameters.
-        //This can be used for a "thorns" effect.
-    }
+        public void OnDamaged(int damage)
+        {
+            //This is called when the player is damaged.
+            //The damage that is dealt will be passed through the parameters.
+            //This can be used for a "thorns" effect.
+        }
 
-    public void OnEnemyDeath()
-    {
-        //This is called when an enemy dies.
-        //This can be used for a looting effect.
+        public void OnEnemyDeath()
+        {
+            //This is called when an enemy dies.
+            //This can be used for a looting effect.
+        }
     }
 }
